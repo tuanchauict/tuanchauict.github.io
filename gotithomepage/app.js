3f4d49c0bc00c6a1b7a97da0a7d4593ab952acfc
Tuan Chau <tuanchau.ict@gmail.com>
2017-12-23 17:12:15 +0700

    Update app.js

--- gotithomepage/app.js
+++ gotithomepage/app.js
@@ -1,7 +1,26 @@
 const app = new Una({
     el: 'app',
     data: {
-        activeTab: 0,
+        activeTab: (() => {
+            console.log(window.location.search);
+            let tab = window.location.search.match(/tab=(.+?)(?:&|$)/);
+            if (!tab || tab.length < 2){
+                return 0
+            }
+            tab = tab[1];
+            const map = {
+                '': 0,
+                'kaas': 1,
+                'solutions': 2,
+                'about': 3,
+                'team': 4,
+                'experts': 5,
+            };
+            if (map.hasOwnProperty(tab)) {
+                return map[tab];
+            }
+            return 0;
+        })(),
         tabs: [
             {title: 'Home'},
             {title: 'KaaS Platform'},
@@ -14,6 +33,18 @@ const app = new Una({
     methods: {
         active: function(index){
             this.activeTab = index;
+            console.log(this.activeTab);
+            const map = {
+                0: '',
+                1: '?tab=kaas',
+                2: '?tab=solutions',
+                3: '?tab=about',
+                4: '?tab=team',
+                5: '?tab=experts',
+            };
+
+            history.pushState({}, null, window.location.pathname + map[index]);
+
         }
     }
 });
