b79442a98f87466c8f245be85a5d90957c3bddb3
Tuan Chau <tuanchau.ict@gmail.com>
2017-12-24 14:42:08 +0700

    Update app.js

--- gotithomepage/app.js
+++ gotithomepage/app.js
@@ -3,22 +3,24 @@ const app = new Una({
     data: {
         activeTab: (() => {
             let tab = window.location.search.match(/tab=(.+?)(?:&|$)/);
-            if (!tab || tab.length < 2) {
-                return 0
+            const titles = ['Got It', 'KaaS Platform', 'Solutions', 'About', 'Team', 'Experts'];
+            let index = 0;
+            if (tab && tab.length >= 2){
+                tab = tab[1];
+                const map = {
+                    '': 0,
+                    'kaas': 1,
+                    'solutions': 2,
+                    'about': 3,
+                    'team': 4,
+                    'experts': 5,
+                };
+                if (map.hasOwnProperty(tab)) {
+                    index = map[tab];
+                }
             }
-            tab = tab[1];
-            const map = {
-                '': 0,
-                'kaas': 1,
-                'solutions': 2,
-                'about': 3,
-                'team': 4,
-                'experts': 5,
-            };
-            if (map.hasOwnProperty(tab)) {
-                return map[tab];
-            }
-            return 0;
+            document.title = titles[index];
+            return index;
         })(),
         tabs: [
             {title: 'Home'},
@@ -32,17 +34,11 @@ const app = new Una({
     methods: {
         active: function (index) {
             this.activeTab = index;
-            const map = {
-                0: '',
-                1: '?tab=kaas',
-                2: '?tab=solutions',
-                3: '?tab=about',
-                4: '?tab=team',
-                5: '?tab=experts',
-            };
-
-            history.pushState({}, null, window.location.pathname + map[index]);
+            const params = ['', '?tab=kaas', '?tab=solutions', '?tab=about', '?tab=team', '?tab=experts'];
+            const titles = ['Got It', 'KaaS Platform', 'Solutions', 'About', 'Team', 'Experts'];
+            history.pushState({}, titles[index], window.location.pathname + params[index]);
             window.scroll(0, 0);
+            document.title= titles[index];
         }
     }
 });
