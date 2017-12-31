78454eab810c6eb6e9bd35d70fe53f1cea610d88
Tuan Chau <tuanchauict@gmail.com>
2017-12-31 20:39:49 +0700

    Init banggia2

---
+++ banggia2/public/js/app.js
@@ -0,0 +1,14 @@
+class PriceTable extends React.Component {
+    
+    
+    render() {
+        return (
+            <div>Bang gia</div>
+        );
+    }
+}
+
+
+
+
+ReactDOM.render(<PriceTable/>, document.getElementById('content'));
